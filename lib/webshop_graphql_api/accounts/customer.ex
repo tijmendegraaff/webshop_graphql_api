e0f812fb7a391adb4b6688411d07f457e416dbbd
Tijmen de Graaff <tijmen76@gmail.com>
2019-05-25 22:48:25 +0200

    add validation on customer model

--- lib/webshop_graphql_api/accounts/customer.ex
+++ lib/webshop_graphql_api/accounts/customer.ex
@@ -2,10 +2,14 @@ defmodule WebshopGraphqlApi.Accounts.Customer do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias WebshopGraphqlApi.Validators.EmailValidator
+
   schema "customers" do
     field :email, :string
     field :first_name, :string
     field :last_name, :string
+    field :password, :string, virtual: true
+    field :password_confirmation, :string, virtual: true
     field :password_hash, :string
     field :phone_number, :string
 
@@ -20,14 +24,19 @@ defmodule WebshopGraphqlApi.Accounts.Customer do
       :last_name,
       :email,
       :phone_number,
-      :password_hash
+      :password
     ])
     |> validate_required([
       :first_name,
       :last_name,
       :email,
       :phone_number,
-      :password_hash
+      :password
     ])
+    |> EmailValidator.validate_email(:email)
+    |> update_change(:email, &String.downcase(&1))
+    |> validate_length(:password, min: 6, max: 100)
+    |> validate_confirmation(:password, message: "passwords don't match")
+    |> unique_constraint(:email)
   end
 end
