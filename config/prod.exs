4383a414e4568eebfe501916473b70a56067d75c
Tijmen de Graaff <tijmen76@gmail.com>
2019-05-26 17:23:08 +0200

    setup guardian in dev/ prod and module

--- config/prod.exs
+++ config/prod.exs
@@ -21,6 +21,10 @@ config :webshop_graphql_api, WebshopGraphqlApi.Repo,
   ssl: true,
   url: System.get_env("DATABASE_URL")
 
+config :webshop_graphql_api, WebshopGraphqlApi.Guardian,
+  issuer: "webshop_graphql_api",
+  secret_key: System.get_env("GUARDIAN_SECRET")
+
 # Do not print debug messages in production
 config :logger, level: :info
 
