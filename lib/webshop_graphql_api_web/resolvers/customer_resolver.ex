55076c4bc763e892c27623499138e2e39b60545f
Tijmen de Graaff <tijmen76@gmail.com>
2019-05-26 20:58:40 +0200

    put sessions in assigned resolvers for model

--- lib/webshop_graphql_api_web/resolvers/customer_resolver.ex
+++ lib/webshop_graphql_api_web/resolvers/customer_resolver.ex
@@ -1,5 +1,7 @@
 defmodule WebshopGraphqlApiWeb.Resolvers.CustomerResolver do
   alias WebshopGraphqlApi.Accounts
+  alias WebshopGraphqlApi.Accounts.Session
+  alias WebshopGraphqlApi.Guardian
 
   def customers(_, _, _) do
     {:ok, Accounts.list_customers()}
@@ -7,5 +9,12 @@ defmodule WebshopGraphqlApiWeb.Resolvers.CustomerResolver do
 
   def create_customer(_, %{input: input}, _) do
     Accounts.create_customer(input)
+  end
+
+  def create_customer_session(_, %{input: input}, _) do
+    with {:ok, customer} <- Session.authenticate_customer(input),
+         {:ok, jwt, _} <- Guardian.encode_and_sign(customer) do
+      {:ok, %{customer: customer, token: jwt}}
+    end
   end
 end
