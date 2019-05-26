d04be0287e63307ccb00c412e65d03041feaf367
Tijmen de Graaff <tijmen76@gmail.com>
2019-05-26 12:43:34 +0200

    add validation. to employee changeset

--- lib/webshop_graphql_api/accounts/employee.ex
+++ lib/webshop_graphql_api/accounts/employee.ex
@@ -2,11 +2,16 @@ defmodule WebshopGraphqlApi.Accounts.Employee do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias WebshopGraphqlApi.Validators.EmailValidator
+  alias WebshopGraphqlApi.Utils.HashPassword
+
   schema "employees" do
     field :email, :string
     field :first_name, :string
     field :last_name, :string
     field :password_hash, :string
+    field :password, :string, virtual: true
+    field :password_confirmation, :string, virtual: true
     field :phone_number, :string
     field :role, :string
 
@@ -21,16 +26,23 @@ defmodule WebshopGraphqlApi.Accounts.Employee do
       :last_name,
       :email,
       :phone_number,
-      :password_hash,
+      :password,
+      :password_confirmation,
       :role
     ])
     |> validate_required([
       :first_name,
       :last_name,
       :email,
-      :phone_number,
-      :password_hash,
+      :password,
+      :password_confirmation,
       :role
     ])
+    |> EmailValidator.validate_email(:email)
+    |> update_change(:email, &String.downcase(&1))
+    |> validate_length(:password, min: 6, max: 100)
+    |> validate_confirmation(:password, message: "passwords don't match")
+    |> unique_constraint(:email)
+    |> HashPassword.hash_password()
   end
 end
