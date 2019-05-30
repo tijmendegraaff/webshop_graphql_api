44ebacc78752f87e697f58e66999c35d716666ef
Tijmen de Graaff <tijmen76@gmail.com>
2019-05-30 19:36:12 +0200

    add customer seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,42 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias WebshopGraphqlApi.Repo
+alias WebshopGraphqlApi.Accounts.{Employee, Customer, Address}
+
+#
+# CUSTOMERS
+#
+
+tijmen =
+  %Customer{}
+  |> Customer.changeset(%{
+    first_name: "Tijmen",
+    last_name: "de Graaff",
+    email: "tijmen@example.com",
+    password: "password12345",
+    password_confirmation: "password12345"
+  })
+  |> Repo.insert!()
+
+merel =
+  %Customer{}
+  |> Customer.changeset(%{
+    first_name: "Merel",
+    last_name: "de Graaff",
+    email: "merel@example.com",
+    password: "password12345",
+    password_confirmation: "password12345"
+  })
+  |> Repo.insert!()
+
+floortje =
+  %Customer{}
+  |> Customer.changeset(%{
+    first_name: "Floortje",
+    last_name: "de Graaff",
+    email: "floortje@example.com",
+    password: "password12345",
+    password_confirmation: "password12345"
+  })
+  |> Repo.insert!()
